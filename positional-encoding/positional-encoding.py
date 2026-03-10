87cfdf2ebee1a157561e770d2a13f22e6b3e9199
Lê Anh Duy <153476871+Le-Anh-Duy@users.noreply.github.com>
2026-03-10 15:15:17 +0700

    Update solution for positional-encoding

--- positional-encoding/positional-encoding.py
+++ positional-encoding/positional-encoding.py
@@ -15,7 +15,7 @@ def positional_encoding(seq_len, d_model, base=10000.0):
     pos = np.arange(0, seq_len)
     
     v = np.outer(pos, b)
-    print(v.shape)
+    # print(v.shape)
 
     v[::,0:d_model:2] = np.sin(v[::,0:d_model:2])
     v[::,1:d_model:2] = np.cos(v[::,1:d_model:2])
