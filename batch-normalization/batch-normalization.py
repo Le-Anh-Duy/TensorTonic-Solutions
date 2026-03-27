780691177cd4a78cb74102b1139ea0bd9b7e29b3
Lê Anh Duy <153476871+Le-Anh-Duy@users.noreply.github.com>
2026-03-27 12:00:00 +0700

    Add solution for batch-normalization

---
+++ batch-normalization/batch-normalization.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def batch_norm_forward(input, gamma, beta, eps=1e-5):
+    """
+    Forward-only BatchNorm for (N,D) or (N,C,H,W).
+    """
+    # Write code here
+    # pass
+
+    x = np.array(input)
+    x_hat = np.zeros(x.shape)
+    if len(x.shape) == 2:
+        for i in range(x.shape[1]):
+            u = np.mean(x[:,i])
+            s2 = np.mean((x[:,i] - u) ** 2)
+            x_hat[:,i] = gamma[i] * (x[:,i] - u) / np.sqrt(s2 + eps) + beta[i]
+    else:
+        for i in range(x.shape[1]):
+            u = np.mean(x[:,i,:,:])
+            s2 = np.mean((x[:,i,:,:] - u) ** 2)
+            x_hat[:,i] = gamma[i] * (x[:,i,:,:] - u) / np.sqrt(s2 + eps) + beta[i]
+            
+    return x_hat
