448b1536ff5495cb88aca755de69c824b1d3fdfd
Lê Anh Duy <153476871+Le-Anh-Duy@users.noreply.github.com>
2026-04-13 14:29:57 +0700

    Add solution for expected-calibration-error

---
+++ expected-calibration-error/expected-calibration-error.py
@@ -0,0 +1,19 @@
+import math
+def expected_calibration_error(y_true, y_pred, n_bins):
+    """
+    Compute Expected Calibration Error.
+    """
+    # Write code here
+    bins = [0 for i in range(n_bins)]
+    n = len(y_true)
+    for y, p in zip(y_true, y_pred):
+        idx = math.floor(p * n_bins)
+        idx -= idx == n_bins
+        bins[idx] += 1.0 * (y - p) / n
+
+    print(bins)
+
+    ans = sum([abs(bins[idx]) for idx in range(n_bins)])
+    
+
+    return ans
